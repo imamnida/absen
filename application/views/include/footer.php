6af97e8c75b9a4f1924da5a712459df2d0f59da0
imam dienul <imamdienul@imams-MacBook-Air.local>
2024-01-02 15:38:41 +0700

    first init github

---
+++ application/views/include/footer.php
@@ -0,0 +1,11 @@
+<?php
+
+?>
+  <footer class="main-footer">
+    <div class="pull-right hidden-xs">
+      <b>Version</b> 1.0
+    </div>
+    <strong>Copyright &copy; 2019 <a href="<?=base_url();?>">Tyto Mulyono</a>.</strong> All rights
+    reserved.
+    
+  </footer>
