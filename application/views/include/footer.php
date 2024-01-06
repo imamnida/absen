57a45908ea42d442bee54500b5adda7e6c236335
Salimam <154986548+Salmaimam@users.noreply.github.com>
2024-01-06 17:14:53 +0700

    Update footer.php

--- application/views/include/footer.php
+++ application/views/include/footer.php
@@ -5,7 +5,7 @@
     <div class="pull-right hidden-xs">
       <b>Version</b> 1.0
     </div>
-    <strong>Copyright &copy; 2019 <a href="<?=base_url();?>">Tyto Mulyono</a>.</strong> All rights
+    <strong>Copyright &copy; 2019 <a href="<?=base_url();?>">Imam Dienul B</a>.</strong> All rights
     reserved.
     
   </footer>
