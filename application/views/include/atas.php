7a17a421c79a917b411855373048ecc816c6243f
Salimam <154986548+Salmaimam@users.noreply.github.com>
2024-01-13 22:00:57 +0700

    Update atas.php

--- application/views/include/atas.php
+++ application/views/include/atas.php
@@ -1,4 +1,5 @@
 <?php
+
 if($this->session->userdata('userlogin'))     // mencegah akses langsung tanpa login
 { 
   $users = $this->session->userdata('userlogin');
@@ -11,23 +12,23 @@ if($this->session->userdata('userlogin'))     // mencegah akses langsung tanpa l
 ?>
 <!DOCTYPE html>
 <html lang="en">
-
+ 
 <head>
   <meta charset="utf-8">
   <meta content="width=device-width, initial-scale=1.0" name="viewport">
-
+ 
   <title>Admin Dashboard</title>
   <meta content="" name="description">
   <meta content="" name="keywords">
-
+ 
   <!-- Favicons -->
   <link href="<?=base_url();?>imam/assets/img/favicon.png" rel="icon">
   <link href="<?=base_url();?>imam/assets/img/apple-touch-icon.png" rel="apple-touch-icon">
-
+ 
   <!-- Google Fonts -->
   <link href="https://fonts.gstatic.com" rel="preconnect">
   <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Nunito:300,300i,400,400i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
-
+ 
   <!-- Vendor CSS Files -->
   <link href="<?=base_url();?>imam/assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
   <link href="<?=base_url();?>imam/assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
@@ -36,10 +37,10 @@ if($this->session->userdata('userlogin'))     // mencegah akses langsung tanpa l
   <link href="<?=base_url();?>imam/assets/vendor/quill/quill.bubble.css" rel="stylesheet">
   <link href="<?=base_url();?>imam/assets/vendor/remixicon/remixicon.css" rel="stylesheet">
   <link href="<?=base_url();?>imam/assets/vendor/simple-datatables/style.css" rel="stylesheet">
-
+ 
   <!-- Template Main CSS File -->
   <link href="<?=base_url();?>imam/assets/css/style.css" rel="stylesheet">
-
+ 
   <!-- =======================================================
   * Template Name: NiceAdmin
   * Updated: Jan 09 2024 with Bootstrap v5.3.2
@@ -47,13 +48,23 @@ if($this->session->userdata('userlogin'))     // mencegah akses langsung tanpa l
   * Author: BootstrapMade.com
   * License: https://bootstrapmade.com/license/
   ======================================================== -->
+ 
+  <style>
+    .nav-link.active,  .nav-link.active i {
+      color : #4154f1 !important;
+    }
+    .sidebar-nav .nav-content a.active {
+      color : #4154f1 !important;
+ 
+    }
+  </style>
 </head>
-
+ 
 <body>
-
+ 
   <!-- ======= Header ======= -->
   <header id="header" class="header fixed-top d-flex align-items-center">
-
+ 
     <div class="d-flex align-items-center justify-content-between">
       <a href="index.html" class="logo d-flex align-items-center">
         <img src="<?=base_url();?>imam/assets/img/logo.png" alt="">
@@ -61,39 +72,39 @@ if($this->session->userdata('userlogin'))     // mencegah akses langsung tanpa l
       </a>
       <i class="bi bi-list toggle-sidebar-btn"></i>
     </div><!-- End Logo -->
-
+ 
     <div class="search-bar">
       <form class="search-form d-flex align-items-center" method="POST" action="#">
         <input type="text" name="query" placeholder="Search" title="Enter search keyword">
         <button type="submit" title="Search"><i class="bi bi-search"></i></button>
       </form>
     </div><!-- End Search Bar -->
-
+ 
     <nav class="header-nav ms-auto">
       <ul class="d-flex align-items-center">
-
+ 
         <li class="nav-item d-block d-lg-none">
           <a class="nav-link nav-icon search-bar-toggle " href="#">
             <i class="bi bi-search"></i>
           </a>
         </li><!-- End Search Icon-->
         <li class="nav-item dropdown">
-
+ 
 <a class="nav-link nav-icon" href="<?=base_url();?>admin/setting">
   <i class="bi bi-gear"></i>
   
 </a><!-- End Notification Icon -->
 </li><!-- End Notification Nav -->
-
+ 
        
        
         <li class="nav-item dropdown pe-3">
-
+ 
           <a class="nav-link nav-profile d-flex align-items-center pe-0" href="#" data-bs-toggle="dropdown">
             <img src="<?=base_url();?>components/dist/img/<?=$avatar;?>" class="img-circle" alt="User Image">
             <span class="d-none d-md-block dropdown-toggle ps-2"> <?=$this->session->userdata('userlogin');?></span>
           </a><!-- End Profile Iamge Icon -->
-
+ 
           <ul class="dropdown-menu dropdown-menu-end dropdown-menu-arrow profile">
             <li class="dropdown-header">
               <h6> <?=$this->session->userdata('userlogin');?></h6>
@@ -102,98 +113,98 @@ if($this->session->userdata('userlogin'))     // mencegah akses langsung tanpa l
             <li>
               <hr class="dropdown-divider">
             </li>
-
+ 
             <li>
               
-
+ 
             
-
+ 
             <li>
               <a class="dropdown-item d-flex align-items-center" href="<?=base_url();?>login/logout">
                 <i class="bi bi-box-arrow-right"></i>
                 <span>Sign Out</span>
               </a>
             </li>
-
+ 
           </ul><!-- End Profile Dropdown Items -->
         </li><!-- End Profile Nav -->
-
+ 
       </ul>
     </nav><!-- End Icons Navigation -->
-
+ 
   </header><!-- End Header -->
-
+ 
   <!-- ======= Sidebar ======= -->
   <aside id="sidebar" class="sidebar">
-
+ 
     <ul class="sidebar-nav" id="sidebar-nav">
-
+ 
       <li class="nav-item">
-        <a <?php if($this->uri->segment(2)=="dashboard"){echo 'class="nav-link"';}?>" href="<?=base_url();?>admin/dashboard">
+        <a class="nav-link <?php if($this->uri->segment(2)=="dashboard"){echo 'active';}?> collapsed" href="<?=base_url();?>admin/dashboard">
           <i class="bi bi-grid"></i>
           <span>Dashboard</span>
         </a>
       </li><!-- End Dashboard Nav -->
-
+ 
       
-
+ 
       <li class="nav-item">
-        <a class="nav-link collapsed" href="<?=base_url();?>admin/list_users">
+        <a class="nav-link  <?php if($this->uri->segment(2)=="list_users"){echo 'active';}?> collapsed" href="<?=base_url();?>admin/list_users">
           <i class="bi bi-person"></i>
           <span>Daftar User</span>
         </a>
       </li><!-- End Profile Page Nav -->
-
+ 
       <li class="nav-item">
-        <a class="nav-link collapsed" href="<?=base_url();?>admin/devices">
+        <a class="nav-link <?php if($this->uri->segment(2)=="devices"){echo 'active';}?>  collapsed" href="<?=base_url();?>admin/devices">
           <i class="ri-equalizer-line"></i>
           <span>Data Alat</span>
         </a>
       </li><!-- End F.A.Q Page Nav -->
-
+ 
       <li class="nav-item">
-        <a class="nav-link collapsed" href="<?=base_url();?>admin/kelas">
+        <a class="nav-link  <?php if($this->uri->segment(2)=="kelas"){echo 'active';}?> collapsed" href="<?=base_url();?>admin/kelas">
           <i class="ri-building-2-line"></i>
           <span>Daftar Kelas</span>
         </a>
       </li><!-- End Contact Page Nav -->
-
+ 
       <li class="nav-item">
-        <a class="nav-link collapsed" data-bs-target="#forms-nav" data-bs-toggle="collapse" href="#">
+        <a class="nav-link  collapsed " data-bs-target="#forms-nav" data-bs-toggle="collapse" href="#">
           <i class="bi bi-journal-text"></i><span>Data RFID</span><i class="bi bi-chevron-down ms-auto"></i>
         </a>
-        <ul id="forms-nav" class="nav-content collapse " data-bs-parent="#sidebar-nav">
+        <ul id="forms-nav" class="nav-content collapse <?php if($this->uri->segment(2)=="rfid"){echo 'show';}?>" data-bs-parent="#sidebar-nav">
           <li>
-            <a href="<?=base_url();?>admin/rfid/rfidnew">
+            <a class="<?php if($this->uri->segment(3)=="rfidnew"){echo 'active';}?>" href="<?=base_url();?>admin/rfid/rfidnew">
               <i class="bi bi-circle"></i><span>RFID NEW</span>
             </a>
           </li>
           <li>
-            <a href="<?=base_url();?>admin/rfid/datarfid">
+            <a class="<?php if($this->uri->segment(3)=="datarfid"){echo 'active';}?>" href="<?=base_url();?>admin/rfid/datarfid">
               <i class="bi bi-circle"></i><span>RFID</span>
             </a>
           </li>
           
         </ul>
       </li><!-- End Forms Nav -->
-
+ 
       <li class="nav-item">
-        <a class="nav-link collapsed" href="<?=base_url();?>admin/absensi">
+        <a class="nav-link <?php if($this->uri->segment(2)=="absensi"){echo 'active';}?> collapsed" href="<?=base_url();?>admin/absensi">
           <i class="bi bi-box-arrow-in-right"></i>
           <span>Absensi</span>
         </a>
       </li><!-- End Login Page Nav -->
-
+ 
       <li class="nav-item">
-        <a class="nav-link collapsed" href="<?=base_url();?>admin/histori">
+        <a class="nav-link  <?php if($this->uri->segment(2)=="histori"){echo 'active';}?>  collapsed" href="<?=base_url();?>admin/histori">
           <i class="ri-compass-2-line"></i>
           <span>Histori Alat</span>
         </a>
       </li><!-- End Error 404 Page Nav -->
-
+ 
       
-
+ 
     </ul>
-
+ 
   </aside>
   <!-- End Sidebar-->
