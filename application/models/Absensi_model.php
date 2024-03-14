7a1837088ec43ed89339c1e5e33924cfb41a92f3
Salimam <154986548+Salmaimam@users.noreply.github.com>
2024-03-14 08:47:18 +0700

    Update Absensi_model.php

--- application/models/Absensi_model.php
+++ application/models/Absensi_model.php
@@ -8,53 +8,61 @@ class Absensi_model extends CI_Model {
     }
 
     public function absen_masuk($uid, $id_devices) {
-        $id_rfid = $this->get_id_rfid_by_uid($uid);
-        $data = array(
-            'id_devices' => $id_devices,
-            'id_rfid' => $id_rfid,
-            'keterangan' => 'masuk',
-            'foto' => '', 
-            'created_at' => time() 
-        );
-        $this->db->insert('absensi', $data);
+        if (!$this->is_already_absent($uid, 'masuk')) {
+            $id_rfid = $this->get_id_rfid_by_uid($uid);
+            $data = array(
+                'id_devices' => $id_devices,
+                'id_rfid' => $id_rfid,
+                'keterangan' => 'masuk',
+                'foto' => '',
+                'created_at' => time(),
+                'is_absent_today' => 1  // Tandai bahwa siswa sudah absen hari ini
+            );
+            $this->db->insert('absensi', $data);
+        } else {
+            // Siswa sudah melakukan absensi masuk hari ini
+            // Tambahkan log, tampilkan pesan, atau lakukan tindakan sesuai kebutuhan Anda
+        }
     }
 
     public function absen_keluar($uid, $id_devices) {
-        $id_rfid = $this->get_id_rfid_by_uid($uid);
-        $data = array(
-            'id_devices' => $id_devices,
-            'id_rfid' => $id_rfid,
-            'keterangan' => 'keluar',
-            'foto' => '',
-            'created_at' => time()
-        );
-        $this->db->insert('absensi', $data);
+        if (!$this->is_already_absent($uid, 'keluar')) {
+            $id_rfid = $this->get_id_rfid_by_uid($uid);
+            $data = array(
+                'id_devices' => $id_devices,
+                'id_rfid' => $id_rfid,
+                'keterangan' => 'keluar',
+                'foto' => '',
+                'created_at' => time(),
+                'is_absent_today' => 1  // Tandai bahwa siswa sudah absen hari ini
+            );
+            $this->db->insert('absensi', $data);
+        } else {
+            // Siswa sudah melakukan absensi keluar hari ini
+            // Tambahkan log, tampilkan pesan, atau lakukan tindakan sesuai kebutuhan Anda
+        }
+    }
+
+    public function is_already_absent($uid, $keterangan) {
+        $today = date("Y-m-d");
+        $this->db->where('id_rfid', $this->get_id_rfid_by_uid($uid));
+        $this->db->where('keterangan', $keterangan);
+        $this->db->where('DATE(created_at)', $today);
+        $this->db->where('is_absent_today', 1); // Memeriksa apakah siswa sudah absen hari ini
+        $query = $this->db->get('absensi');
+        return $query->num_rows() > 0;
+    }
+
+    public function is_registered_uid($uid) {
+        $this->db->where('uid', $uid);
+        $query = $this->db->get('rfid');
+        return $query->num_rows() > 0;
     }
 
     private function get_id_rfid_by_uid($uid) {
         $query = $this->db->get_where('rfid', array('uid' => $uid));
         $result = $query->row();
         return $result ? $result->id_rfid : null;
-    }
-
-    public function is_already_absent($uid, $keterangan) {
-    $today = date("Y-m-d"); 
-
- 
-    $this->db->where('id_rfid', $this->get_id_rfid_by_uid($uid));
-    $this->db->where('keterangan', $keterangan);
-    $this->db->where('DATE(created_at)', $today); 
-    $query = $this->db->get('absensi');
-    
-
-    return $query->num_rows() > 0;
-}
-
-
-    public function is_registered_uid($uid) {
-        $this->db->where('uid', $uid);
-        $query = $this->db->get('rfid');
-        return $query->num_rows() > 0;
     }
 }
 ?>
