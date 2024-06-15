4e77d132945d80314cc3259556cf9c068bf7b7f5
Salimam <154986548+Salmaimam@users.noreply.github.com>
2024-06-15 18:48:23 +0700

    Update i_dashboard.php

--- application/views/i_dashboard.php
+++ application/views/i_dashboard.php
@@ -138,7 +138,7 @@ if ($set == "dashboard") {
                             </div>
                             <div class="col-8 ml-auto align-self-center text-center">
                                 <div class="m-l-10 text-white float-right">
-                                    <h5 class="mt-0 round-inner">0</h5>
+                                    <h5 class="mt-0 round-inner">1</h5>
                                     <p class="mb-0">Siswa Sakit</p>
                                 </div>
                             </div>
