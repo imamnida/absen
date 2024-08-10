cd950fa0dea7e488a7466316dedf68b4164d224a
Salimam <154986548+Salmaimam@users.noreply.github.com>
2024-08-10 20:38:31 +0700

    contoh
    contoh

--- application/controllers/Register.php
+++ application/controllers/Register.php
@@ -7,92 +7,47 @@ class Register extends CI_Controller {
         parent::__construct();
         $this->load->model('RfidModel');
         $this->load->helper(array('form', 'url'));
-        $this->load->library('form_validation');
+        $this->load->library('upload');
     }
 
     public function index() {
         $data['kelas'] = $this->RfidModel->get_kelas();
         $data['kampus'] = $this->RfidModel->get_kampus();
+
         $this->load->view('i_rfid_registration', $data);
     }
 
-    public function register() {
-        $this->form_validation->set_rules('nis', 'NIS', 'required');
-        $this->form_validation->set_rules('nama', 'Nama', 'required');
-        $this->form_validation->set_rules('telp', 'Telepon', 'required');
-        $this->form_validation->set_rules('gender', 'Gender', 'required');
-        $this->form_validation->set_rules('alamat', 'Alamat', 'required');
-        $this->form_validation->set_rules('id_kelas', 'Kelas', 'required');
-        $this->form_validation->set_rules('id_kampus', 'Kampus', 'required');
-        $this->form_validation->set_rules('penyakit', 'Penyakit', 'required');
-        $this->form_validation->set_rules('nomerortu', 'Nomor Orang Tua', 'required');
-        $this->form_validation->set_rules('foto', 'Foto', 'callback_file_check');
-        $this->form_validation->set_rules('kaka', 'Kartu Keluarga', 'callback_file_check');
-        $this->form_validation->set_rules('rumah', 'Foto Rumah', 'callback_file_check');
+    public function submit() {
+        $config['upload_path'] = './uploads/';
+        $config['allowed_types'] = 'gif|jpg|png|jpeg';
+        $config['max_size'] = 2048;
+        $this->upload->initialize($config);
 
-        if ($this->form_validation->run() == FALSE) {
-            $this->index();
+        if (!$this->upload->do_upload('foto')) {
+            $error = array('error' => $this->upload->display_errors());
+            $data['kelas'] = $this->RfidModel->get_kelas();
+            $data['kampus'] = $this->RfidModel->get_kampus();
+            $data['upload_error'] = $error;
+            $this->load->view('i_rfid_registration', $data);
         } else {
-            // Upload files
-            $foto = $this->do_upload('foto');
-            $kaka = $this->do_upload('kaka');
-            $rumah = $this->do_upload('rumah');
+            $upload_data = $this->upload->data();
+            $original_file_name = $upload_data['file_name'];
+            $new_file_name = $this->input->post('nama') . $upload_data['file_ext'];
+
+            // Rename the file
+            rename('./uploads/' . $original_file_name, './uploads/' . $new_file_name);
 
             $data = array(
-                'nis' => $this->input->post('nis'),
                 'nama' => $this->input->post('nama'),
-                'telp' => $this->input->post('telp'),
-                'gender' => $this->input->post('gender'),
+                'ttl' => $this->input->post('tempat_tanggal_lahir'),
+                'nisn' => $this->input->post('nisn'),
                 'alamat' => $this->input->post('alamat'),
-                'id_kelas' => $this->input->post('id_kelas'),
-                'id_kampus' => $this->input->post('id_kampus'),
-                'penyakit' => $this->input->post('penyakit'),
-                'nomerortu' => $this->input->post('nomerortu'),
-                'foto' => $foto,
-                'kaka' => $kaka,
-                'rumah' => $rumah
+                'foto' => $new_file_name,
             );
 
             $this->RfidModel->insert_rfid($data);
-            redirect('register/index');
-        }
-    }
-
-    public function file_check($str) {
-        $allowed_mime_type_arr = array('image/jpeg','image/jpg','image/png','image/gif');
-        if (isset($_FILES[$str]['name']) && $_FILES[$str]['name'] != "") {
-            $mime = get_mime_by_extension($_FILES[$str]['name']);
-            if (in_array($mime, $allowed_mime_type_arr)) {
-                return true;
-            } else {
-                $this->form_validation->set_message('file_check', 'Please select only jpg/png/gif file.');
-                return false;
-            }
-        } else {
-            $this->form_validation->set_message('file_check', 'Please choose a file to upload.');
-            return false;
-        }
-    }
-
-    private function do_upload($field) {
-        $config['upload_path'] = './uploads/';
-        $config['allowed_types'] = 'gif|jpg|png';
-        $config['max_size'] = 2048;
-        $config['max_width'] = 1024;
-        $config['max_height'] = 768;
-        $config['file_name'] = $field . '_' . time();
-
-        $this->load->library('upload', $config);
-
-        if (!$this->upload->do_upload($field)) {
-            $error = $this->upload->display_errors();
-            log_message('error', $error);
-            $this->form_validation->set_message('file_check', $error);
-            return '';
-        } else {
-            return $this->upload->data('file_name');
+            redirect('register');
         }
     }
 }
-
 ?>
--- application/views/i_rfid_registration.php
+++ application/views/i_rfid_registration.php
@@ -4,15 +4,14 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0, minimal-ui">
-    <title>DAFTAR</title>
+    <title>Daftar</title>
     <meta content="Admin Dashboard" name="description">
     <meta content="Mannatthemes" name="author">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <link rel="shortcut icon" href="<?=base_url();?>assets/images/logo.png">
-    <link href="<?=base_url();?>assets/plugins/animate/animate.css" rel="stylesheet" type="text/css">
-    <link href="<?=base_url();?>assets/css/bootstrap-material-design.min.css" rel="stylesheet" type="text/css">
-    <link href="<?=base_url();?>assets/css/icons.css" rel="stylesheet" type="text/css">
-    <link href="<?=base_url();?>assets/css/style.css" rel="stylesheet" type="text/css">
+    <link rel="shortcut icon" href="<?= base_url(); ?>assets/images/logo.png">
+    <link href="<?= base_url(); ?>assets/plugins/animate/animate.css" rel="stylesheet" type="text/css">
+    <link href="<?= base_url(); ?>assets/css/bootstrap-material-design.min.css" rel="stylesheet" type="text/css">
+    <link href="<?= base_url(); ?>assets/css/icons.css" rel="stylesheet" type="text/css">
+    <link href="<?= base_url(); ?>assets/css/style.css" rel="stylesheet" type="text/css">
     <style>
         .wrapper-page {
             display: flex;
@@ -23,6 +22,23 @@
         .card {
             width: 100%;
             max-width: 600px;
+            padding: 20px;
+            border: 1px solid #ddd;
+            border-radius: 5px;
+        }
+        .card-body {
+            padding: 20px;
+        }
+        .text-center img {
+            height: 150px;
+        }
+        .btn-primary {
+            background-color: #007bff;
+            border-color: #007bff;
+        }
+        .btn-primary:hover {
+            background-color: #0056b3;
+            border-color: #004085;
         }
     </style>
 </head>
@@ -36,100 +52,43 @@
                     <div class="card-body">
                         <div class="text-center pt-3">
                             <a href="#">
-                                <img src="<?=base_url();?>assets/images/logo.png" alt="logo" height="200">
+                                <img src="<?= base_url(); ?>assets/images/logo.png" alt="logo">
                             </a>
                         </div>
                         <div class="p-3">
                             <?php echo validation_errors(); ?>
-                            <?php echo form_open_multipart('register/register'); ?>
-                                <div class="form-group row">
-                                    <div class="col-12">
-                                        <input class="form-control" type="text" name="nis" id="nis" required placeholder="NIS">
-                                    </div>
+                            <?php echo form_open_multipart('register/submit'); ?>
+                                <div class="form-group">
+                                    <label for="nama">Nama:</label>
+                                    <small class="form-text text-muted">Contoh: IMAM DIENUL BAYAN</small>
+                                    <input class="form-control" type="text" name="nama" id="nama" required placeholder="Nama">
                                 </div>
-                                <div class="form-group row">
-                                    <div class="col-12">
-                                        <input class="form-control" type="text" name="nama" id="nama" required placeholder="Nama">
-                                    </div>
+                                <div class="form-group">
+                                    <label for="tempat_tanggal_lahir">Tempat, Tanggal Lahir:</label>
+                                    <small class="form-text text-muted">Contoh: Majalengka, 29 Agustus 2005</small>
+                                    <input class="form-control" type="text" name="tempat_tanggal_lahir" id="tempat_tanggal_lahir" required placeholder="Tempat, Tanggal Lahir">
                                 </div>
-                                <div class="form-group row">
-                                    <div class="col-12">
-                                        <input class="form-control" type="text" name="telp" id="telp" required placeholder="Telepon">
-                                    </div>
+                                <div class="form-group">
+                                    <label for="nisn">NISN:</label>
+                                    <small class="form-text text-muted">Contoh: 1212838392</small>
+                                    <input class="form-control" type="text" name="nisn" id="nisn" required placeholder="NISN">
                                 </div>
-                                <div class="form-group row">
-                                    <div class="col-12">
-                                        <select class="form-control" name="gender" id="gender" required>
-                                            <option value="">Pilih Gender</option>
-                                            <option value="L">Laki-Laki</option>
-                                            <option value="P">Perempuan</option>
-                                        </select>
-                                    </div>
+                                <div class="form-group">
+                                    <label for="alamat">Alamat:</label>
+                                    <small class="form-text text-muted">Contoh: Desa Cibeureum, Kec. Talaga, Kab. Majalengka</small>
+                                    <input class="form-control" type="text" name="alamat" id="alamat" required placeholder="Alamat">
                                 </div>
-                                <div class="form-group row">
-                                    <div class="col-12">
-                                        <input class="form-control" type="text" name="alamat" id="alamat" required placeholder="Alamat">
-                                    </div>
-                                </div>
-                                <div class="form-group row">
-                                    <div class="col-12">
-                                        <select class="form-control" name="id_kelas" id="id_kelas" required>
-                                            <option value="">Pilih Kelas</option>
-                                            <?php foreach ($kelas as $k): ?>
-                                                <option value="<?php echo $k->id; ?>"><?php echo $k->kelas; ?></option>
-                                            <?php endforeach; ?>
-                                        </select>
-                                    </div>
-                                </div>
-                                <div class="form-group row">
-                                    <div class="col-12">
-                                        <select class="form-control" name="id_kampus" id="id_kampus" required>
-                                            <option value="">Pilih Kampus</option>
-                                            <?php foreach ($kampus as $k): ?>
-                                                <option value="<?php echo $k->id; ?>"><?php echo $k->kampus; ?></option>
-                                            <?php endforeach; ?>
-                                        </select>
-                                    </div>
-                                </div>
-                                <div class="form-group row">
-                                    <div class="col-12">
-                                        <input class="form-control" type="text" name="penyakit" id="penyakit" required placeholder="Penyakit">
-                                    </div>
-                                </div>
-                                <div class="form-group row">
-                                    <div class="col-12">
-                                        <input class="form-control" type="text" name="nomerortu" id="nomerortu" required placeholder="Nomor Orang Tua">
-                                    </div>
-                                </div>
-                                <div class="form-group row">
-                                    <div class="col-12">
-                                        <label for="foto">Foto:</label>
-                                        <input class="form-control" type="file" name="foto" id="foto" required>
-                                    </div>
-                                </div>
-                                <div class="form-group row">
-                                    <div class="col-12">
-                                        <label for="kaka">Kartu Keluarga:</label>
-                                        <input class="form-control" type="file" name="kaka" id="kaka" required>
-                                    </div>
-                                </div>
-                                <div class="form-group row">
-                                    <div class="col-12">
-                                        <label for="rumah">Foto Rumah:</label>
-                                        <input class="form-control" type="file" name="rumah" id="rumah" required>
-                                    </div>
+                                <div class="form-group">
+                                    <label for="foto">Foto:</label>
+                                    <input class="form-control" type="file" name="foto" id="foto" required>
                                 </div>
                                 <!-- Submit button -->
-                                <div class="form-group text-center row m-t-20">
-                                    <div class="col-12">
-                                        <button class="btn btn-raised btn-primary btn-block waves-effect waves-light" type="submit">Register</button>
-                                    </div>
+                                <div class="form-group text-center">
+                                    <button class="btn btn-primary btn-block waves-effect waves-light" type="submit">Register</button>
                                 </div>
                             <?php echo form_close(); ?>
-                            <div class="form-group m-t-10 mb-0 row">
-                                <div class="col-12 m-t-20 text-center">
-                                    <a href="#" class="text-muted">Already have account?</a>
-                                </div>
+                            <div class="form-group text-center">
+                                <a href="#" class="text-muted">Already have an account?</a>
                             </div>
                         </div>
                     </div>
@@ -138,18 +97,18 @@
         </div>
     </div>
     <!-- jQuery -->
-    <script src="<?=base_url();?>assets/js/jquery.min.js"></script>
-    <script src="<?=base_url();?>assets/js/popper.min.js"></script>
-    <script src="<?=base_url();?>assets/js/bootstrap-material-design.js"></script>
-    <script src="<?=base_url();?>assets/js/modernizr.min.js"></script>
-    <script src="<?=base_url();?>assets/js/detect.js"></script>
-    <script src="<?=base_url();?>assets/js/fastclick.js"></script>
-    <script src="<?=base_url();?>assets/js/jquery.slimscroll.js"></script>
-    <script src="<?=base_url();?>assets/js/jquery.blockUI.js"></script>
-    <script src="<?=base_url();?>assets/js/waves.js"></script>
-    <script src="<?=base_url();?>assets/js/jquery.nicescroll.js"></script>
-    <script src="<?=base_url();?>assets/js/jquery.scrollTo.min.js"></script>
+    <script src="<?= base_url(); ?>assets/js/jquery.min.js"></script>
+    <script src="<?= base_url(); ?>assets/js/popper.min.js"></script>
+    <script src="<?= base_url(); ?>assets/js/bootstrap-material-design.js"></script>
+    <script src="<?= base_url(); ?>assets/js/modernizr.min.js"></script>
+    <script src="<?= base_url(); ?>assets/js/detect.js"></script>
+    <script src="<?= base_url(); ?>assets/js/fastclick.js"></script>
+    <script src="<?= base_url(); ?>assets/js/jquery.slimscroll.js"></script>
+    <script src="<?= base_url(); ?>assets/js/jquery.blockUI.js"></script>
+    <script src="<?= base_url(); ?>assets/js/waves.js"></script>
+    <script src="<?= base_url(); ?>assets/js/jquery.nicescroll.js"></script>
+    <script src="<?= base_url(); ?>assets/js/jquery.scrollTo.min.js"></script>
     <!-- App js -->
-    <script src="<?=base_url();?>assets/js/app.js"></script>
+    <script src="<?= base_url(); ?>assets/js/app.js"></script>
 </body>
 </html>
