b4de766696f47aa331bb35756a87845a76189e3f
imam <imamdienul@gmail.com>
2024-10-06 23:59:30 +0700

    Update i_promo.php

--- application/views/i_promo.php
+++ application/views/i_promo.php
@@ -5,7 +5,7 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Absensi-Gisaka Media</title>
+  <title>Absensi|Gisaka Media</title>
   <!-- -------- anime css ------ -->
   <link rel="stylesheet" href="<?php base_url()?>promo/css/animate.css">
   <!-- --------- tiny slider css ------ -->
